4a839a1ffff6648a13f41f17af25e5bc999f5a22
joros96 <joros96@gmail.com>
2016-09-06 13:17:27 -0500

    HelloWorldJonathanO
    Hello World Program

---
+++ initializevariables/src/main/java/com/example/MyClass.java
@@ -0,0 +1,13 @@
+// Program: Initialize Variables
+// Programmer: Jonathan Oros
+// Purpose:
+// Date: 09/06/2016
+package com.example;
+
+public class MyClass
+{
+    public static void main(String[] args)
+    {
+        byte
+    }
+}
